5c66b72540833b774f47fedef795404f9939d032
samskinnerphd <samskinnerphd@gmail.com>
2017-07-05 14:06:51 -0400

    tweak Welcome views

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -6,6 +6,16 @@ class WelcomeController < ApplicationController
   end
 
   def about_params
+
     params[:color] == nil ? @color = 'orange' : @color = params[:color]
+
+    # if params[:color] == nil
+    #   @color = 'orange'
+    # else
+    #   @color = params[:color]
+    # end
+
+    @size = params[:shoe_size]
+
   end
 end
